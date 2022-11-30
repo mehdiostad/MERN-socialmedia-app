fe0eba26d919a3a1977c0fc1e5b48f85523fa2d3
mehdi <me.ostad80@gmail.com>
2022-11-30 16:03:59 +0330

    change socket io adrress

--- client/src/Pages/Chat/Chat.jsx
+++ client/src/Pages/Chat/Chat.jsx
@@ -31,7 +31,7 @@ const Chat = () => {
   }, [user]);
 
   useEffect(()=>{
-    socket.current = io('http://localhost:8800')
+    socket.current = io('http://51.89.107.233/socket.io')
     socket.current.emit('new-user-add', user._id)
     socket.current.on("get-users" , users => {
       setOnlineUsers(users)
